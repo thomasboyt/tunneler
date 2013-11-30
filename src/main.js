706253451cd6ee70491619cfaf8fd477b2a2e22b
Thomas Boyt <me@thomasboyt.com>
2013-11-30 01:27:42 -0500

    es6ify Game and Tunnel classes

--- src/main.js
+++ src/main.js
@@ -3,34 +3,35 @@ import Enemy from "sound-and-vision/enemy";
 import Player from "sound-and-vision/player";
 module c from "sound-and-vision/constants";
 
-var Game = function(canvasId, width, height) {
-  window.coq = new Coquette(this, canvasId, width, height, "#000");
+class Game {
+  constructor(canvasId, width, height) {
+    window.coq = new Coquette(this, canvasId, width, height, "#000");
 
-  this.sides = 8;
+    this.sides = 8;
 
-  coq.entities.create(Tunnel);
+    coq.entities.create(Tunnel);
 
-  coq.entities.create(Enemy, {
-    sector: Math.floor(Math.random() * this.sides),
-    zindex: 2
-  });
-
-  coq.entities.create(Player, {
-    zindex: 1
-  });
-
-  this.spawnTimerInc = 0;
-
-};
-
-Game.prototype.update = function(dt) {
-  this.spawnTimerInc += dt;
-  if (this.spawnTimerInc > 300) {
     coq.entities.create(Enemy, {
       sector: Math.floor(Math.random() * this.sides),
+      zindex: 2
     });
+
+    coq.entities.create(Player, {
+      zindex: 1
+    });
+
     this.spawnTimerInc = 0;
   }
-};
+
+  update(dt) {
+    this.spawnTimerInc += dt;
+    if (this.spawnTimerInc > 300) {
+      coq.entities.create(Enemy, {
+        sector: Math.floor(Math.random() * this.sides),
+      });
+      this.spawnTimerInc = 0;
+    }
+  }
+}
 
 new Game("container", c.CANVAS_WIDTH, c.CANVAS_HEIGHT);
--- src/tunnel.js
+++ src/tunnel.js
@@ -3,53 +3,55 @@ import TunnelLine from 'sound-and-vision/line';
 
 module c from "sound-and-vision/constants";
 
-var Tunnel = function(game, settings) {
-  this.game = game;
-  this.sides = game.sides;
+class Tunnel {
+  constructor(game, settings) {
+    this.game = game;
+    this.sides = game.sides;
 
-  this.generateGridlineTimer = 0;
-};
-
-Tunnel.prototype.update = function(dt) {
-  this.generateGridlineTimer += dt;
-
-  if (this.generateGridlineTimer > 1000) {
     this.generateGridlineTimer = 0;
+  }
 
-    // generate a tunnel line for each side
-    for (var i = 0; i < this.sides; i++) {
-      coq.entities.create(TunnelLine, {
-        color: 'white',
-        sector: i
-      });
+  update(dt) {
+    this.generateGridlineTimer += dt;
+
+    if (this.generateGridlineTimer > 1000) {
+      this.generateGridlineTimer = 0;
+
+      // generate a tunnel line for each side
+      for (var i = 0; i < this.sides; i++) {
+        coq.entities.create(TunnelLine, {
+          color: 'white',
+          sector: i
+        });
+      }
     }
   }
-};
 
-Tunnel.prototype.draw = function(ctx) {
-  ctx.strokeStyle = '#fff';
-  ctx.fillStyle = '#fff';
-  ctx.lineWidth = 1;
+  draw(ctx) {
+    ctx.strokeStyle = '#fff';
+    ctx.fillStyle = '#fff';
+    ctx.lineWidth = 1;
 
-  ctx.save();
+    ctx.save();
 
-  var rotDeg = (180 + (this.sides - 3) * 180) / this.sides / 2;
-  var rot = Math.PI/180 * rotDeg;
+    var rotDeg = (180 + (this.sides - 3) * 180) / this.sides / 2;
+    var rot = Math.PI/180 * rotDeg;
 
-  ctx.translate(c.ORIGIN_X, c.ORIGIN_Y);
-  ctx.rotate(rot);
-  ctx.translate(-c.ORIGIN_X, -c.ORIGIN_Y);
+    ctx.translate(c.ORIGIN_X, c.ORIGIN_Y);
+    ctx.rotate(rot);
+    ctx.translate(-c.ORIGIN_X, -c.ORIGIN_Y);
 
-  // enclosing shape
-  this.outerPts = drawRegularPolygon(ctx, this.sides, c.OUTER_RADIUS, c.ORIGIN_X, c.ORIGIN_Y);
+    // enclosing shape
+    this.outerPts = drawRegularPolygon(ctx, this.sides, c.OUTER_RADIUS, c.ORIGIN_X, c.ORIGIN_Y);
 
-  // center shape
-  this.centerPts = drawRegularPolygon(ctx, this.sides, c.INNER_RADIUS, c.ORIGIN_X, c.ORIGIN_Y);
+    // center shape
+    this.centerPts = drawRegularPolygon(ctx, this.sides, c.INNER_RADIUS, c.ORIGIN_X, c.ORIGIN_Y);
 
-  // draw lines from center to outer pts
-  drawConnectingLines(ctx, this.outerPts, this.centerPts);
-  
-  ctx.restore();
-};
+    // draw lines from center to outer pts
+    drawConnectingLines(ctx, this.outerPts, this.centerPts);
+    
+    ctx.restore();
+  }
+}
 
 export default Tunnel;
